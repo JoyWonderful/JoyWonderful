f8aadb6ac7b3d058f554557cf60fbf3340c6eb6c
jywondf <99643089+JoyWonderful@users.noreply.github.com>
2024-08-09 10:54:15 +0800

    Add luogu-discuss-time.js in adguard-user-filter

---
+++ one-file-object/luogu-dis-time.js
@@ -0,0 +1,18 @@
+(function() {
+    var b = new URL(window.location);
+    if(b.pathname.slice(0, 8) == "/discuss") {
+        window.addEventListener("load", (function() {
+            var a = document.querySelectorAll(".time");
+            a.forEach((e) => {
+                var c = e.querySelector("time[datetime]")
+                if(c) {
+                    const d = new Date(c.getAttribute("datetime"))
+                    c.innerHTML = new Intl.DateTimeFormat("zh-CN", {year:"numeric", month:"2-digit", day:"2-digit", hour:"2-digit", minute:"2-digit", second:"2-digit"}).format(d);
+                }
+                else if(e.hasAttribute("title")) {
+                    e.innerHTML = e.getAttribute("title");
+                }
+            });
+        }));
+    }
+})();
